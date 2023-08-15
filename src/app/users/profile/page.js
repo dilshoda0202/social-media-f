d535e0de7ce8ed186b5bbc1ce06699d16f4a5665
Dilshoda Abdualimova <dilshoda@Dilshodas-MacBook-Pro.local>
2023-08-15 12:36:28 -0700

    feat: profile page edit

--- src/app/users/profile/page.js
+++ src/app/users/profile/page.js
@@ -1,5 +1,5 @@
 'use client'
-import 'bootstrap/dist/css/bootstrap.css';
+
 import { useState, useEffect } from 'react';
 import jwtDecode from 'jwt-decode';
 import { useRouter } from 'next/navigation';
@@ -49,60 +49,52 @@ export default function Profile() {
     if (!data) return <p>No data shown...</p>;
 
     return (
-        <div className="container" style={{ background: "linear-gradient(to bottom, gold, silver)", height: '100vh' }}>
+        <div className="flex items-stretch justify-center gap-40 h-screen w-full">
             <div className="main-body">
-                <nav aria-label="breadcrumb" className="main-breadcrumb">
-                    <ol className="breadcrumb">
-                        <li className="breadcrumb-item"><a href="/">Home</a></li>
-                        <li className="breadcrumb-item"><a href="/users/profile">Profile</a></li>
-                        <li className="breadcrumb-item"><a href="/users/edit">Edit Profile</a></li>
-                        <li className="breadcrumb-item"><a href="/users/events-2">Event List</a></li>
-                        <li className="breadcrumb-item"><a href="/users/events-2/new">Create Event</a></li>
-                        <li className="breadcrumb-item" onClick={handleLogout}><a href="">Logout</a></li>
-                    </ol>
-                </nav>
-
                 <div className="row gutters-sm">
                     <div className="col-md-4 mb-3">
-                        <div className="card">
+                        <div className="container mx-auto">
                             <div className="card-body">
                                 <div className="d-flex flex-column align-items-center text-center">
                                     <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
                                     <div className="mt-3">
-                                        {/* <h4>{data.firstName} {data.lastName}</h4> */}
+                                        <h4>{data.firstName} {data.lastName}</h4>
                                         <p className="text-secondary mb-1">{data.jobTitle}</p>
                                         {/* <p className="text-muted font-size-sm">{data.address.city}, {data.address.state}</p> */}
-                                        <button className="btn btn-primary">Follow</button>
-                                        <button className="btn btn-outline-primary">Message</button>
+                                        <button type="submit" className="bg-blue-600 text-white rounded-md py-3 w-full">Follow</button>
+                                        <button type="submit" className="bg-blue-600 text-white rounded-md py-3 w-full">Message</button>
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-8">
-                        <div className="card mb-3">
+                    <div className="container mx-auto">
+                        <div className="container mx-auto">
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col-sm-3">
-                                        {/* <h6 className="mb-0">Full Name</h6> */}
+                                        <h6 className="mb-0">Name: </h6>
                                     </div>
-                                    {/* <div className="col-sm-9 text-secondary">
-                    {data.firstName} {data.lastName} */}
+                                    <div className="col-sm-9 text-secondary">
+                                        {data.firstName} {data.lastName}
+                                    </div>
                                 </div>
-                            </div>
-                            <hr />
-                            <div className="row">
-                                <div className="col-sm-3">
-                                    <h6 className="mb-0">Email</h6>
+                                <hr />
+                                <div className="row">
+                                    <div className="col-sm-3">
+                                        <h6 className="mb-0">Email: </h6>
+                                    </div>
+                                    <div className="col-sm-9 text-secondary">
+                                        {data.email}
+                                    </div>
                                 </div>
-                                <div className="col-sm-9 text-secondary">
-                                    {data.email}
-                                </div>
-                            </div>
-                            <hr />
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <a className="btn btn-info " target="__blank" href="/users/edit">Edit</a>
+                                <hr />
+                                <div className="row">
+                                    <div className="col-sm-12">
+                                        <button type="submit" className="bg-blue-600 text-white rounded-md py-3 w-full">Edit</button>
+                                        {/* <a className="btn btn-info " target="__blank" href="/users/edit">Edit</a> */}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -110,6 +102,5 @@ export default function Profile() {
                 </div>
             </div>
         </div>
-        // </div>
     );
 }
