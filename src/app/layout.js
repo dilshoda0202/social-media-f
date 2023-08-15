6362ec10f5feb17f9329e4557bcd7ff4ec98ef39
Dilshoda Abdualimova <dilshoda@Dilshodas-MacBook-Pro.local>
2023-08-15 15:01:11 -0700

    feat: update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,24 @@ function defaultLayout(children) {
 function authenticatedLayout(children) {
     return (
         <div className="container mx-auto mt-10">
+            <header className="bg-blue-500 p-4">
+                <div className="container mx-auto flex items-center justify-between">
+                    <div className="flex items-center space-x-4">
+                        <img
+                            src="/icon.svg"
+                            alt="Logo"
+                            className="w-14 h-14"
+                        />
+                        <h1 className="text-white text-lg font-semibold">Mom Network</h1>
+                    </div>
+                </div>
+            </header>
+            <br></br>
             <div className="flex gap-10">
                 <div className="flex flex-col gap-8 border border-gray-300 rounded-md w-1/3 p-6">
                     <div className="flex gap-2 hover:bg-gray-100 rounded-md p-2.5">
                         <House size={24}></House>
-                        <a href="/">Home</a>
+                        <Link href="/">Home</Link>
                     </div>
                     <div className="flex gap-2 hover:bg-gray-100 rounded-md p-2.5">
                         <User size={24}></User>
@@ -36,7 +49,6 @@ function authenticatedLayout(children) {
                         <SignOut size={24}></SignOut>
                         <Link href="/users/logout/">Logout</Link>
                     </div>
-
                 </div>
                 <div className="w-2/3">
                     {children}
